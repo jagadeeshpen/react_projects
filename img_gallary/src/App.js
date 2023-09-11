e2d376cc734d5c55c547196e0f8a2db2244a48c4
jagadeeshp <jagadeeshpennupothu487@gmail.com>
2023-09-11 18:45:14 +0530

    changed app content in both

--- img_gallary/src/App.js
+++ img_gallary/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          img_gallary
         </a>
       </header>
     </div>
--- todo_list/src/App.js
+++ todo_list/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          todo_list
         </a>
       </header>
     </div>
